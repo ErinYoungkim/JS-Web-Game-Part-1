05a1752e89fed297040190b6b97430ec4017df31
Erin Y Kim <erinykim04@gmail.com>
2023-11-29 18:13:43 -0800

    Finished wo bonus

--- index.js
+++ index.js
@@ -15,11 +15,28 @@ function newItem(url, left, bottom) {
         object.remove()
     })
 }
+
 newImage('assets/green-character.gif', 100, 100);
-newImage('assets/pine-tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
+newImage('assets/pine-tree.png', 200, 300);
+newImage('assets/pillar.png', 350, 100);
+newImage('assets/crate.png', 150, 200);
+newImage('assets/well.png', 500, 425);
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png ', 600, 100)
+
+
+// let sword = document.createElement('img')
+// sword.src = 'assets/sword.png'
+// sword.position = 'fixed'
+// sword.left = '500px'
+// sword.bottom = '405px'
+// document.body.append(sword)
+
+// sword.addEventListener('click', function(){
+//     sword.remove()
+// })
 
 // let greenCharacter = document.createElement('img')
 // greenCharacter.src = 'assets/green-character.gif'
